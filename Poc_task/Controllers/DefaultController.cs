e8e26dceef4a1e67b65dac1da7f58cb6138295cc
Rashmita Piplani <rashmita.piplani@unthinkable.co>
2021-12-02 14:53:22 +0530

    Create, Update

--- Poc_task/Controllers/DefaultController.cs
+++ Poc_task/Controllers/DefaultController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Poc_task.Models;
 
 namespace Poc_task.Controllers
 {
@@ -13,5 +14,103 @@ namespace Poc_task.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Index(Employee emp)
+        { 
+        //{  if(ModelState.IsValid)
+        //    {
+                
+        //        return Content(emp.FirstName);
+        //    }
+        //    return View("Index");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                   EmployeeDBContext EmpRepo = new EmployeeDBContext();
+
+                    if (EmpRepo.AddEmployee(emp))
+                    {
+                        ViewBag.Message = "Employee details added successfully";
+                    }
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+        public ActionResult Bootstrap()
+        {
+            return View();
+        }
+        public ActionResult ViewRecords()
+        {
+            EmployeeDBContext db = new EmployeeDBContext();
+            List<Employee> obj = db.GetEmployees();
+
+            return View(obj);
+
+        }
+        public ActionResult CreateRecord()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateRecord(Employee emp)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    EmployeeDBContext EmpRepo = new EmployeeDBContext();
+
+                    if (EmpRepo.AddEmployee(emp))
+                    {
+                        ViewBag.Message = "Employee details added successfully";
+                    }
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+            
+        }
+        [HttpGet]
+        public ActionResult DeleteRecord(string empcode)
+        {
+          //  EmployeeDBContext EmpRepo = new EmployeeDBContext();
+           // var row = EmpRepo.GetEmployees().Find(model => model.EmpCode == empcode);
+           return RedirectToAction("Index");
+            
+        }
+        [HttpPost]
+        public ActionResult DeleteRecord(String empcode,Employee emp)
+        {
+            EmployeeDBContext EmpRepo = new EmployeeDBContext();
+            bool check = EmpRepo.DeleteEmployee(empcode);
+            if(check==true)
+            {
+                TempData["DeleteMessage"] = "Data Has Been Deleted Successfully";
+                ModelState.Clear();
+                return RedirectToAction("CreateRecord");
+
+            }
+            return View();
+
+
+
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
     }
 }
